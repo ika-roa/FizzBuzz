fbae4b0919fb3682ab2d8169af82c55afd5839b2
Stefanie Mak <mak@dynamic-biosensors.com>
2022-11-07 14:28:45 +0100

    refactor: rename fizzbuzz function in tests

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,30 +1,30 @@
 import unittest
-from scripts.fizzbuzz import fizzbuzz
+from scripts.fizzbuzz import calculate_fizzbuzz
 
 
 class TestFizzBuzz(unittest.TestCase):
 
     def test_fizzbuzz_returns_1_for_1(self):
         i = 1
-        assert fizzbuzz(i) == i
+        assert calculate_fizzbuzz(i) == i
 
     def test_fizzbuzz_returns_2_for_2(self):
         i = 2
-        assert fizzbuzz(i) == i
+        assert calculate_fizzbuzz(i) == i
 
     def test_fizzbuzz_returns_fizz_for_3(self):
         i = 3
-        assert fizzbuzz(i) == "Fizz"
+        assert calculate_fizzbuzz(i) == "Fizz"
 
     def test_fizzbuzz_returns_4_for_4(self):
         i = 4
-        assert fizzbuzz(i) == i
+        assert calculate_fizzbuzz(i) == i
 
     def test_fizzbuzz_returns_buzz_for_5(self):
         i = 5
-        assert fizzbuzz(i) == "Buzz"
+        assert calculate_fizzbuzz(i) == "Buzz"
 
     def test_fizzbuzz_returns_fizzbuzz_for_15(self):
         i = 15
-        assert fizzbuzz(i) == "FizzBuzz"
+        assert calculate_fizzbuzz(i) == "FizzBuzz"
 
