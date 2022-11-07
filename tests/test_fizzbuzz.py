87b40d2456df30125f62608f7f9051fd8efa462d
Stefanie Mak <mak@dynamic-biosensors.com>
2022-11-07 13:57:14 +0100

    test: Add test for number not divisible by 3 or 5

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,2 +1,10 @@
 import unittest
 from scripts.fizzbuzz import fizzbuzz
+
+
+class TestFizzBuzz(unittest.TestCase):
+
+    def test_fizzbuzz_returns_1_for_1(self):
+        i = 1
+        assert fizzbuzz(i) == i
+        
