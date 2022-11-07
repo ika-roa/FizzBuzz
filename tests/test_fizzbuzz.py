7d5bbe0f795070d00395323129767c97c7c769dc
Stefanie Mak <mak@dynamic-biosensors.com>
2022-11-07 14:00:53 +0100

    test: Add test for other number not divisible by 3 or 5

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -7,4 +7,8 @@ class TestFizzBuzz(unittest.TestCase):
     def test_fizzbuzz_returns_1_for_1(self):
         i = 1
         assert fizzbuzz(i) == i
-        
+
+    def test_fizzbuzz_returns_2_for_2(self):
+        i = 2
+        assert fizzbuzz(i) == i
+
